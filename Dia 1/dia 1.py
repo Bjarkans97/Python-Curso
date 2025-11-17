83f85084696fc15eb3e9d35c9c2a393f66adbaaa
Bjarkans <benjamin.nramirezs@gmail.com>
2025-11-17 17:59:39 -0300

    Creación de repositorio - subida dia 1

---
+++ Dia 1/dia 1.py
@@ -0,0 +1,17 @@
+## Declaracion de print
+print("Me encanta estudiar Python")
+
+
+## Strings
+
+#Realiza un salto de linea
+print('Línea 1\nLínea 2\nLínea 3')
+
+#Realiza tabulaciones con salto de linea cada 3 caracteres
+print('A\tB\tC\nD\tE\tF\nG\tH\tI')
+
+#Barra invertida en python
+print('Barra Normal: /\nBarra Invertida: \\')
+
+## Input en python
+print(input('¿Qué estás estudiando?'))
